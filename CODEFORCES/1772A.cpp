c30c27db037105e5766c7551839a303597f3cf47
Ashutosh Jha <105535224+the-alone-musk@users.noreply.github.com>
2023-01-18 12:27:23 +0530

    Add files via upload

---
+++ CODEFORCES/1772A.cpp
@@ -0,0 +1,32 @@
+// A.A + B ? 
+// time limit per test2 seconds
+//                   memory limit per test512 megabytes
+//                       inputstandard input
+//                           outputstandard output
+//   are given an expression of the form a +b,
+//     where a and b are integers from 0 to 9.You have to evaluate it and print the result.
+
+//         Input
+//             The first line contains one integer t(1≤t≤100) — the number of test cases.
+
+//         Each test case consists of one line containing an expression of the form a
+//         + b(0≤a, b≤9, both a and b are integers).The integers are not separated from the +
+//         sign.
+
+//         Output
+//         For each test case,
+//     print one integer — the result of the expression.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+while (t!=0){
+ int 
+    t--;
+}
+
+    return 0; 
+}
