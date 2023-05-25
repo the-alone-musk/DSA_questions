c0b69b4c1461bbf651a432c4faf0c7e2c8f9fd8c
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-05-25 12:03:50 +0530

    +1

---
+++ cses/no_of_bits.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void count(int n)
+{
+   int count=0; 
+
+   for ( int i = 1 ; i <= n ; i++){
+    count += __builtin_popcount(i);
+   }
+   cout<<count;
+}
+int main()
+{
+
+    int n;
+    cin >> n;
+    count(n);
+    return 0;
+}
