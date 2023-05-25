b0ce98ca62bc6eb531485a1ed3734a04901696ae
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-05-25 12:13:15 +0530

    push

--- cses/no_of_bits.cpp
+++ cses/no_of_bits.cpp
@@ -3,18 +3,46 @@
 
 using namespace std;
 
-void count(int n)
-{
-   int count=0; 
+// void count(long long int n)
+// {
+//   long long int count=0; 
 
-   for ( int i = 1 ; i <= n ; i++){
-    count += __builtin_popcount(i);
-   }
-   cout<<count;
+//    for ( long long int i = 1 ; i <= n ; i++){
+//     count += __builtin_popcount(i);
+//    }
+//    cout<<count;
+// }
+// int main()
+// {
+//     long long int n;
+//     cin >> n;
+//     count(n);
+//     return 0;
+// }
+
+int countSetBits(long long int n)
+{
+    int count = 0;
+    while (n) {
+        n &= (n - 1);
+        count++;
+    }
+    return count;
 }
+
+void count(long long int n)
+{
+    long long int count = 0;
+
+    for (long long int i = 1; i <= n; i++) {
+        count += countSetBits(i);
+    }
+    cout << count;
+}
+
 int main()
 {
-    int n;
+    long long int n;
     cin >> n;
     count(n);
     return 0;
